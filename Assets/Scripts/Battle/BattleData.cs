c2961b27043e8c59065044217b012dfff7bcc06e
Ofsouzap <ofsouzap@gmail.com>
2021-02-27 22:04:44 +0000

    Basic BattleData properties

--- Assets/Scripts/Battle/BattleData.cs
+++ Assets/Scripts/Battle/BattleData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Battle;
 
@@ -7,8 +5,33 @@ namespace Battle
 {
     public class BattleData
     {
-        
 
+        public BattleParticipantPlayer participantPlayer;
+        public BattleParticipant participantOpponent;
+
+        #region Weather
+
+        /// <summary>
+        /// The id of the current weather
+        /// </summary>
+        public int currentWeatherId;
+
+        /// <summary>
+        /// Whether the weather has been changed into what it is now or whether the current weather is the initial weather. This determines whether the weather should fade back to the initial weather or not
+        /// </summary>
+        public bool weatherHasBeenChanged;
+
+        /// <summary>
+        /// The numnber of turns until the weather should fade back to the initial weather
+        /// </summary>
+        public byte turnsUntilWeatherFade;
+
+        /// <summary>
+        /// The id of the initial weather. This is what should be faded back to if the weather is changed
+        /// </summary>
+        public int initialWeatherId;
+
+        #endregion
 
     }
 }
