261d9c274decaf5e70ae9bed5cc3892237458ee2
Ofsouzap <ofsouzap@gmail.com>
2021-02-28 00:01:55 +0000

    BattleParticipant and start BattleParticipantNPC

--- Assets/Scripts/Battle/BattleParticipant.cs
+++ Assets/Scripts/Battle/BattleParticipant.cs
@@ -9,6 +9,8 @@ namespace Battle
 
         public BattleManager battleManager;
 
+        #region Choose Action
+
         /// <summary>
         /// Whether the action has been chosen
         /// </summary>
@@ -19,7 +21,42 @@ namespace Battle
         /// </summary>
         public Action chosenAction;
 
-        public abstract void StartChoosingAction();
+        public abstract void StartChoosingAction(BattleData battleData);
+
+        #endregion
+
+        #region Pokemon
+
+        /// <summary>
+        /// The index of the participant's active pokemon in their party
+        /// </summary>
+        public int activePokemonIndex;
+
+        public abstract Pokemon.PokemonInstance[] GetPokemon();
+
+        #endregion
+
+        #region Next Pokemon
+
+        /// <summary>
+        /// Whether the next pokemon has been chosen
+        /// </summary>
+        public bool nextPokemonHasBeenChosen = false;
+
+        /// <summary>
+        /// Index of pokemon chosen to replace current pokemon. This is only valid if nextPokemonHasBeenChosen is true
+        /// </summary>
+        public int chosenNextPokemonIndex;
+
+        public abstract void StartChoosingNextPokemon();
+
+        #endregion
+
+        /// <summary>
+        /// Check if the participant is defeated. This should always be implemented by checking whether all the participant's pokemon have fainted
+        /// </summary>
+        /// <returns>Whether the participant has been defeated</returns>
+        public abstract bool CheckIfDefeated();
 
     }
 }
--- Assets/Scripts/Battle/BattleParticipantNPC.cs
+++ Assets/Scripts/Battle/BattleParticipantNPC.cs
@@ -1,12 +1,117 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Battle;
+using Pokemon;
 
 namespace Battle
 {
     public class BattleParticipantNPC : BattleParticipant
     {
+
+        #region Mode
+
+        public enum Mode
+        {
+            RandomAttack
+            //TODO - add more
+        }
+
+        public Mode mode;
+
+        #endregion
+
+        public PokemonInstance[] pokemon;
+
+        public BattleParticipantNPC(Mode mode,
+            PokemonInstance[] pokemon)
+        {
+            this.mode = mode;
+            this.pokemon = pokemon;
+        }
+
+        public override PokemonInstance[] GetPokemon() => pokemon;
+
+        #region Action Choosing
+
+        public override void StartChoosingAction(BattleData battleData)
+        {
+
+            actionHasBeenChosen = true;
+
+            switch (mode)
+            {
+
+                case Mode.RandomAttack:
+                    chosenAction = ChooseAction_RandomAttack(battleData);
+                    break;
+
+                default:
+                    Debug.LogError("Unknown npc participant mode - " + mode);
+                    actionHasBeenChosen = false;
+                    break;
+
+                //TODO - continue once more added
+
+            }
+
+        }
+
+        public Action ChooseAction_RandomAttack(BattleData battleData)
+        {
+
+            int movesCount = 0;
+            foreach (int moveId in pokemon[activePokemonIndex].moveIds)
+                if (moveId != 0)
+                    movesCount++;
+
+            int chosenMoveIndex = Random.Range(0, movesCount);
+
+            return new Action()
+            {
+                type = Action.Type.Fight,
+                fightMoveIndex = chosenMoveIndex
+            };
+
+        }
+
+        #endregion
+
+        public override void StartChoosingNextPokemon()
+        {
+
+            //All opponents use their pokemon in the order they have them
+
+            int selectedIndex = -1;
+            bool validIndexFound = false;
+
+            for (int i = 0; i < pokemon.Length; i++)
+            {
+
+                if (pokemon[i].health > 0)
+                {
+                    selectedIndex = i;
+                    validIndexFound = true;
+                    break;
+                }
+
+            }
+
+            if (!validIndexFound)
+            {
+                Debug.LogError("Unable to find valid next pokemon index");
+                nextPokemonHasBeenChosen = false;
+                return;
+            }
+
+            chosenNextPokemonIndex = selectedIndex;
+            nextPokemonHasBeenChosen = true;
+
+        }
+
+        public override bool CheckIfDefeated()
+        {
+            return pokemon.All((x) => x.health <= 0);
+        }
 
     }
 }
