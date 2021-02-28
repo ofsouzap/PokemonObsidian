0349e643a51714b7484c23ee41c716de8e805bbe
Ofsouzap <ofsouzap@gmail.com>
2021-02-28 16:09:11 +0000

    Battle fight menu

--- Assets/Scripts/Battle/PlayerBattleUIController.cs
+++ Assets/Scripts/Battle/PlayerBattleUIController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PlayerBattleUIController : MonoBehaviour
 {
