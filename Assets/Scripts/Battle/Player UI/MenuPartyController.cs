def8adf9f55e83e951ca1e6d4f632f3589e2f8b2
Ofsouzap <ofsouzap@gmail.com>
2021-03-09 14:50:43 +0000

    Battle Party Menu navigation fix

--- Assets/Scripts/Battle/Player UI/MenuPartyController.cs
+++ Assets/Scripts/Battle/Player UI/MenuPartyController.cs
@@ -39,7 +39,8 @@ namespace Battle.PlayerUI
         public void RefreshButtons()
         {
 
-            Pokemon.PokemonInstance[] pokemon = PlayerData.singleton.partyPokemon;
+            PlayerData playerData = PlayerData.singleton;
+            Pokemon.PokemonInstance[] pokemon = playerData.partyPokemon;
 
             for (int i = 0; i < pokemon.Length; i++)
             {
@@ -56,6 +57,108 @@ namespace Battle.PlayerUI
                     ((float)pokemon[i].health) / pokemon[i].GetStats().health);
 
             }
+
+            #region Navigation
+
+            Selectable[] buttonDownTargets = new Selectable[4];
+            Button[] buttons = pokemonButtons.Select(x => x.GetComponent<Button>()).ToArray();
+            Selectable buttonBackUpTarget = buttons[4];
+
+            #region Setting Button Down Targets
+
+            if (playerData.GetNumberOfPartyPokemon() > 2)
+            {
+                buttonDownTargets[0] = buttons[2];
+                buttonDownTargets[1] = buttons[3];
+            }
+            else
+            {
+                buttonDownTargets[0] = buttonDownTargets[1] = buttonBack;
+            }
+
+            if (playerData.GetNumberOfPartyPokemon() > 4)
+            {
+                buttonDownTargets[2] = buttons[4];
+                buttonDownTargets[3] = buttons[5];
+            }
+            else
+            {
+                buttonDownTargets[2] = buttonDownTargets[3] = buttonBack;
+            }
+
+            #endregion
+
+            #region Setting Back Button Up Target
+
+            if (playerData.GetNumberOfPartyPokemon() <= 2)
+            {
+                buttonBackUpTarget = buttons[0];
+            }
+            else if (playerData.GetNumberOfPartyPokemon() <= 4)
+            {
+                buttonBackUpTarget = buttons[2];
+            }
+            else
+            {
+                buttonBackUpTarget = buttons[4];
+            }
+
+            #endregion
+
+            #region Setting Button Navigations
+
+            buttons[0].navigation = new Navigation()
+            {
+                mode = Navigation.Mode.Explicit,
+                selectOnUp = buttons[0].navigation.selectOnUp,
+                selectOnRight = buttons[0].navigation.selectOnRight,
+                selectOnLeft = buttons[0].navigation.selectOnLeft,
+                selectOnDown = buttonDownTargets[0]
+            };
+
+            buttons[1].navigation = new Navigation()
+            {
+                mode = Navigation.Mode.Explicit,
+                selectOnUp = buttons[1].navigation.selectOnUp,
+                selectOnRight = buttons[1].navigation.selectOnRight,
+                selectOnLeft = buttons[1].navigation.selectOnLeft,
+                selectOnDown = buttonDownTargets[1]
+            };
+
+            buttons[2].navigation = new Navigation()
+            {
+                mode = Navigation.Mode.Explicit,
+                selectOnUp = buttons[2].navigation.selectOnUp,
+                selectOnRight = buttons[2].navigation.selectOnRight,
+                selectOnLeft = buttons[2].navigation.selectOnLeft,
+                selectOnDown = buttonDownTargets[2]
+            };
+
+            buttons[3].navigation = new Navigation()
+            {
+                mode = Navigation.Mode.Explicit,
+                selectOnUp = buttons[3].navigation.selectOnUp,
+                selectOnRight = buttons[3].navigation.selectOnRight,
+                selectOnLeft = buttons[3].navigation.selectOnLeft,
+                selectOnDown = buttonDownTargets[3]
+            };
+
+            #region Back Button Navigation
+
+            buttonBack.navigation = new Navigation()
+            {
+                mode = Navigation.Mode.Explicit,
+                selectOnUp = buttonBackUpTarget,
+                selectOnRight = buttonBack.navigation.selectOnRight,
+                selectOnLeft = buttonBack.navigation.selectOnLeft,
+                selectOnDown = buttonBack.navigation.selectOnDown
+            };
+
+            #endregion
+
+            #endregion
+
+            #endregion
 
         }
 
