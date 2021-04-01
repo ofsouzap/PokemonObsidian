b277104c5b418615c6171695b91a9219ce5cf14a
Ofsouzap <ofsouzap@gmail.com>
2021-04-01 14:52:14 +0100

    Add poke ball and item sprites
    (Also slightly started abstract Item class)

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,10 +5,36 @@ using Items;
 
 namespace Items
 {
-    public abstract class Item
+    public abstract class Item : IHasId
     {
 
-        //TODO
+        #region Registry
+
+        public static Registry<Item> registry = new Registry<Item>();
+
+        public static Item GetItemById(int id)
+        {
+            return registry.StartingIndexSearch(id, id - 1);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int id;
+        public int GetId() => id;
+
+        /// <summary>
+        /// The name to show to the user for the item
+        /// </summary>
+        public string itemName;
+
+        /// <summary>
+        /// The name of the item's resources used to get its sprite
+        /// </summary>
+        public string resourceName;
+
+        #endregion
 
     }
 }
