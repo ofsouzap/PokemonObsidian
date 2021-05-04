a2998073984234184ec65258c6d221597573297d
Ofsouzap <ofsouzap@gmail.com>
2021-05-04 21:33:35 +0100

    Basic pokemon center
    The pokemon center scene now contains a floor, walls and a counter but still doesn't have any of its needed functionality. This, I will add next

---
+++ Assets/Scripts/FreeRoaming/OccupiedAreaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FreeRoaming
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class OccupiedAreaController : MonoBehaviour, IOccupyPositions
+    {
+
+        public List<Vector2Int> gridPositions { get; protected set; }
+
+        /// <summary>
+        /// The offset from the root of every occupied position of this game object
+        /// </summary>
+        public Vector2Int[] occupiedPositionOffsets = new Vector2Int[0];
+
+        public virtual Vector2Int[] GetPositions()
+        {
+            return gridPositions.ToArray();
+        }
+
+        protected void Start()
+        {
+
+            gridPositions = new List<Vector2Int>();
+            Vector2Int rootGridPosition = Vector2Int.RoundToInt(transform.position);
+
+            if (occupiedPositionOffsets != null)
+                gridPositions.AddRange(occupiedPositionOffsets.Select(x => rootGridPosition + x)); //TODO - test correct posiitons occupied
+
+        }
+
+    }
+}
