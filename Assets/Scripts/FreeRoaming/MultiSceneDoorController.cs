64ee48d9e380461bfd27861e8e6f4fac7e35fd7c
Ofsouzap <ofsouzap@gmail.com>
2021-05-16 13:23:42 +0100

    Area Gates

---
+++ Assets/Scripts/FreeRoaming/MultiSceneDoorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FreeRoaming
+{
+    public class MultiSceneDoorController : MonoBehaviour
+    {
+
+        public SceneDoorDetails doorDetails;
+
+        [SerializeField]
+        private SceneDoor[] doors;
+
+        private void Start()
+        {
+
+            foreach (SceneDoor door in doors)
+                door.doorDetails = doorDetails;
+
+        }
+
+    }
+}
