da174c07b511908bac4dc694c2b10fa300748aaa
ofsouzap <ofsouzap@gmail.com>
2021-08-21 20:57:58 +0100

    Basic message decoration interaction
    Added script for decorations that should display a message when interacted with.
    Fixes #7

---
+++ Assets/Scripts/FreeRoaming/Decorations/BasicMessageDecorationController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FreeRoaming.Decorations
+{
+    public class BasicMessageDecorationController : InteractableDecorationController
+    {
+
+        public string[] messages;
+
+        public override void Interact(GameCharacterController interactee)
+        {
+
+            if (messages.Length > 0)
+                StartCoroutine(MessagesCoroutine());
+
+        }
+
+        protected IEnumerator MessagesCoroutine()
+        {
+
+            bool sceneWasPaused = !sceneController.SceneIsRunning;
+            bool textBoxWasShown = textBoxController.IsShown;
+
+            if (!sceneWasPaused)
+                sceneController.SetSceneRunningState(false);
+
+            if (!textBoxWasShown)
+                textBoxController.Show();
+
+            foreach (string message in messages)
+            {
+                textBoxController.RevealText(message);
+                yield return StartCoroutine(textBoxController.PromptAndWaitUntilUserContinue());
+            }
+
+            if (!sceneWasPaused)
+                sceneController.SetSceneRunningState(true);
+
+            if (!textBoxWasShown)
+                textBoxController.Hide();
+
+        }
+
+    }
+}
