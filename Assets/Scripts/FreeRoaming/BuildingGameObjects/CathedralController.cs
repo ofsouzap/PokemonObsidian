df64c5d5e393276ea17fe79d0bbff80785c9f242
ofsouzap <ofsouzap@gmail.com>
2021-08-21 19:58:25 +0100

    Tildahill
    Added the third town: Tildahill
    Includes a gym (Tildahill gym)

---
+++ Assets/Scripts/FreeRoaming/BuildingGameObjects/CathedralController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FreeRoaming.BuildingGameObjects
+{
+    public class CathedralController : SceneEntranceBuildingController
+    {
+
+        public const string cathedralSceneIdentifier = "Cathedral";
+
+        public override SceneDoorDetails GetBaseSceneDoorDetails()
+            => new SceneDoorDetails()
+            {
+                isDepthLevel = true,
+                isLoadingDoor = true,
+                newSceneTargetPosition = new Vector2Int(0, 0),
+                sceneName = cathedralSceneIdentifier,
+                instanceId = 0
+            };
+
+        public override SceneDoorDetails GetInstanceSpecificSceneDoorDetails(SceneDoorDetails baseDoorDetails)
+            => new SceneDoorDetails()
+            {
+                isDepthLevel = baseDoorDetails.isDepthLevel,
+                isLoadingDoor = baseDoorDetails.isLoadingDoor,
+                newSceneTargetPosition = baseDoorDetails.newSceneTargetPosition,
+                returnPosition = Vector2Int.RoundToInt(door.transform.position) + Vector2Int.down,
+                sceneName = baseDoorDetails.sceneName,
+                instanceId = baseDoorDetails.instanceId
+            };
+
+    }
+}
