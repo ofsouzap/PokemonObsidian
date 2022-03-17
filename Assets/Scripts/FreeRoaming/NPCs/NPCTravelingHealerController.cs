0522f9e0ae1654e9edb92d2faf0e348d62fcbd4a
ofsouzap <ofsouzap@gmail.com>
2022-03-17 19:36:42 +0000

    Traveling Healers
    Added traveling healers: NPCs on long routes that heal the trainer's pokemon
    Also fixed apostrophe character to single quote mark in trainers data file
    Also fixed pokefanf_walk_l_2 sprite

---
+++ Assets/Scripts/FreeRoaming/NPCs/NPCTravelingHealerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using FreeRoaming.Decorations;
+using Audio;
+
+namespace FreeRoaming.NPCs
+{
+    public class NPCTravelingHealerController : NPCGenericTalkController
+    {
+
+        protected string HealingSoundFXResourceName => HealingMachineScript.healingMachineSoundFXResourceName;
+
+        public static readonly string[] defaultMessagesBeforeHeal = new string[]
+        {
+            "Oh, your pokemon look really tired, why don't you let them rest a bit?"
+        };
+
+        public static readonly string[] defaultMessagesAfterHeal = new string[]
+        {
+            "There, all better now."
+        };
+
+        public string[] messagesBeforeHeal = new string[0];
+
+        public string[] messagesAfterHeal = new string[0];
+
+        protected string[] GetMessagesBeforeHeal()
+            => messagesBeforeHeal == null || messagesBeforeHeal.Length == 0 ? defaultMessagesBeforeHeal : messagesBeforeHeal;
+
+        protected string[] GetMessagesAfterHeal()
+            => messagesAfterHeal == null || messagesAfterHeal.Length == 0 ? defaultMessagesAfterHeal : messagesAfterHeal;
+
+        protected override void Start()
+        {
+
+            base.Start();
+
+            dialogs = new string[0];
+
+        }
+
+        public override IEnumerator PlayerInteraction()
+        {
+
+            PlayerData.singleton.HealAllPokemon();
+
+            yield return StartCoroutine(base.PlayerInteraction());
+
+            yield return StartCoroutine(Speak(GetMessagesBeforeHeal()));
+
+            bool readyToContinueAfterSoundFX = false;
+
+            SoundFXController.singleton.PlaySound(HealingSoundFXResourceName,
+                () => readyToContinueAfterSoundFX = true);
+
+            yield return new WaitUntil(() => readyToContinueAfterSoundFX);
+
+            yield return StartCoroutine(Speak(GetMessagesAfterHeal()));
+
+        }
+
+    }
+}
