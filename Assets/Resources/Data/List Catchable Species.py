63ba96c73e04da8b4c7e9a42239469d2ae2961c5
ofsouzap <ofsouzap@gmail.com>
2022-03-20 17:47:26 +0000

    Catchable Species Listing Script
    Added python script to cross-reference wild pokemon areas CSV file and pokemon species CSV file to list all species available in wild pokemon areas and all those that aren't to be used to help decide pokemon to add to future wild pokemon areas.

---
+++ Assets/Resources/Data/List Catchable Species.py
@@ -0,0 +1,59 @@
+import csv;
+
+def load_csv(fn):
+    with open(fn) as f:
+        r = csv.reader(f);
+        ds = list(r);
+    return ds;
+
+def load_wild_area_species(fn):
+
+    ds = load_csv(fn);
+
+    species = set();
+
+    for d in ds[1:]:
+        for x in d[3].split(";"):
+            species.add(int(x));
+
+    return species;
+
+def load_species(fn):
+
+    ds = load_csv(fn);
+
+    species_names = {};
+
+    for d in ds[1:]:
+        species_names[int(d[0])] = d[1];
+
+    return species_names;
+
+def main():
+
+    global MAX_ID;
+
+    species_fn = input("Species CSV Filename> ");
+    wild_area_fn = input("Wild Area CSV Filename> ");
+    
+    species_names = load_species(species_fn);
+    used_species = tuple(sorted(load_wild_area_species(wild_area_fn)));
+
+    # Used Species
+
+    print("Used Species:");
+    for s in used_species:
+        print(f"{s} - {species_names[s]}");
+
+    # Unused Species
+
+    print("-" * 20);
+    print("Unused Species:");
+    for i in sorted(species_names.keys()):
+        if not (i in used_species):
+            print(f"{i} - {species_names[i]}");
+
+    input("Press ENTER to continue...");
+
+if __name__ == "__main__":
+    main();
