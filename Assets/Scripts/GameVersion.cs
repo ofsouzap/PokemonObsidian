105d29ca7bba73027e671d9f6e45dc7b3c648fb2
ofsouzap <ofsouzap@gmail.com>
2022-04-10 19:21:29 +0100

    Set Up for Release
    Set up the project for building the alpha-release by removing debugging objects, setting the game version and setting the player version

--- Assets/Scripts/GameVersion.cs
+++ Assets/Scripts/GameVersion.cs
@@ -2,6 +2,12 @@
 
 public static class GameVersion
 {
+
+    /* Versions:
+     * 0x0000 - v1.0.0a1
+     */
+
     public const ushort version = 0x0000;
     public static byte[] VersionBytes => BitConverter.GetBytes(version);
+
 }
