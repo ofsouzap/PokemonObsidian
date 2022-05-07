678bfa1d3feb667779049155b56a85dfbcb0f6e9
ofsouzap <ofsouzap@gmail.com>
2022-05-08 00:03:41 +0100

    Settings for Beta
    Changed game version in Player settings and GameVersion to reflect this being for the beta release.

--- Assets/Scripts/GameVersion.cs
+++ Assets/Scripts/GameVersion.cs
@@ -5,7 +5,7 @@ public static class GameVersion
 
     /* Versions:
      * 0x0000 - v1.0.0a1-v1.0.0a3
-     * 0x0001 - v1.0.0a4
+     * 0x0001 - v1.0.0a4-v1.0.0b1
      */
 
     public const ushort version = 0x0001;
